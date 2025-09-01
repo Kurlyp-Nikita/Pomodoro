ec1101798df98d6379f65b21684ee97e35e381c8
Nikita <ilinnikita244@gmail.com>
2025-09-01 14:55:05 +0300

    Add task validation and API endpoints for Pomodoro app

--- schema/task.py
+++ schema/task.py
@@ -1,8 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
 
 
 class Task(BaseModel):
     id: int
-    name: str
-    pomodoro_count: int
-    category_id: int
+    name: str | None = None
+    pomodoro_count: int | None = None
+    category_id: int = Field(exclude=True) # исключили это поле
+
+    @model_validator(mode="before")  # @model_validator(mode="before") - валидация из документации.
+    def check_name_or_pomodoro_count_none(self):  # cls - показываем что этот метод класс, @classmethod.
+        if self.name is None and self.pomodoro_count is None:
+            raise ValueError("name or pomodoro_count be provided")
+        return self
