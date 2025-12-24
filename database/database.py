3b4651fbeef8a07ad84b63717b5dabbbc55d1a62
Nikita <ilinnikita244@gmail.com>
2025-12-24 12:13:15 +0300

    add psycopg2

--- database/database.py
+++ database/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("sqlite:///pomodoro.db")
+engine = create_engine("postgresql+psycopg2://postgres:password@0.0.0.0:5432/pomodoro")
+
 Session = sessionmaker(engine)
 
 
