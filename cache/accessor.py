d7ec60c9f253d4f084f2f1277a2def5b4c364a97
Nikita <ilinnikita244@gmail.com>
2025-12-25 20:06:29 +0300

    delete pomodoro_count

--- cache/accessor.py
+++ cache/accessor.py
@@ -3,9 +3,3 @@ import redis
 
 def get_redis_connection() -> redis.Redis:
     return redis.Redis(host='localhost', port=6379, db=0)
-
-
-def set_pomodoro_count():
-    redis = get_redis_connection()
-    redis.set('pomodoro_count', 1)
-
