04479e1f61773f78b554077967b0f4adf570c2e9
Nikita <ilinnikita244@gmail.com>
2026-01-01 18:42:35 +0300

    add in setings.py: CACHE_HOST, CACHE_PORT, CAHE_DB

--- setings.py
+++ setings.py
@@ -6,3 +6,6 @@ class Settings(BaseSettings):
     DB_USER: str = 'postgres'
     DB_PASSWORD: str = 'password'
     DB_NAME: str = 'pomodoro'
+    CACHE_HOST: str = 'localhost'
+    CACHE_PORT: int = 6379
+    CACHE_DB: int = 0
