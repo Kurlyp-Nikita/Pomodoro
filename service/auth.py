26f27ed7b80694504b57425668e80e9a84d24a07
Nikita <ilinnikita244@gmail.com>
2026-01-04 23:27:33 +0300

    add jwt token

--- service/auth.py
+++ service/auth.py
@@ -3,17 +3,23 @@ from models.user import UserProfile
 from repository.user import UserRepository
 from schema.user import UserLoginShema
 from dataclasses import dataclass
+from jose import jwt
+from datetime import timedelta
+import datetime as dt
+
+from settings import Settings
 
 
 @dataclass
 class AuthService:
     user_repository: UserRepository
+    settings: Settings
 
     def login(self, username: str, password: str) -> UserLoginShema:
         user = self.user_repository.get_user_by_username(username)
         self._validate_auth_user(user, password)
-
-        return UserLoginShema(user_id=user.id, access_token=user.access_token)
+        access_token = self.generate_access_token(user_id=user.id)
+        return UserLoginShema(user_id=user.id, access_token=access_token)
 
     @staticmethod
     def _validate_auth_user(user: UserProfile, password: str):
@@ -22,4 +28,13 @@ class AuthService:
 
         if user.password != password:
             raise UserNotCorrectPasswordException
+
+    def generate_access_token(self, user_id: int) -> str:
+        expires_data = (dt.datetime.now(dt.UTC) + timedelta(days=7)).timestamp()
+        token = jwt.encode(
+            {'user_id': user_id, 'expire': expires_data},
+            self.settings.JWT_SECRET_KEY,
+            algorithm=self.settings.JWT_ENCODE_ALGORITHM
+        )
+        return token
 
